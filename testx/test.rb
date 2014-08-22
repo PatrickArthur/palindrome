406d0273b88d0a9b7db0c0f4c5b0c0d9d3ff212f
Patrick <pa@arthurfamily.ws>
2014-08-22 00:48:44 -0400

    refactor

--- testx/test.rb
+++ testx/test.rb
@@ -1,18 +1,28 @@
 def solution(s)
-  str_b='neveroddoreven'
-  if s.length != str_b.length
-    false
-  else
-    counts = Hash.new(0)
-    s.each_char{ |c| counts[c] += 1 }
-    str_b.chars.none?{ |c| (counts[c] -= 1) < 0 }
+  hash = Hash.new(0)
+  s.each_char do |x|
+    hash[x]+=1
   end
-  if false
-    puts 0
-  else
+  odd = []
+  even = []
+  hash.each do |k,v|
+    if v % 2 == 0
+      even << v
+    else
+      odd << v
+    end
+  end
+  if s.length % 2 == 0 && even.empty? == false && odd.empty?
     puts 1
+  elsif s.length % 2 != 0 && odd[0]>1
+    puts 1
+  else
+    puts 0
   end
 end
 
-solution 'dooernedeevrvn'
+solution('aabcba')
+
+
+
 
